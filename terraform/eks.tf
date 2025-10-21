0974451c8de1c5529360b88d100cdcf876b64610
Steven Irvin <stevei101@users.noreply.github.com>
2025-10-21 13:29:52 -0500

    fix(terraform): Add aws-auth configmap for EKS

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -106,6 +106,48 @@ resource "aws_eks_cluster" "eks_cluster" {
   ]
 }
 
+data "aws_eks_cluster_auth" "this" {
+  name = aws_eks_cluster.eks_cluster.name
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = yamlencode([
+      {
+        rolearn  = aws_iam_role.eks_nodegroup_role.arn
+        username = "system:node:{{EC2PrivateDNSName}}"
+        groups = [
+          "system:bootstrappers",
+          "system:nodes",
+        ]
+      },
+      {
+        rolearn  = aws_iam_role.github_actions_role.arn
+        username = "github-actions"
+        groups = [
+          "system:masters",
+        ]
+      }
+    ])
+  }
+
+  depends_on = [
+    aws_eks_node_group.eks_node_group,
+  ]
+}
+
+
 # --- IAM for EKS Node Group ---
 
 resource "aws_iam_role" "eks_nodegroup_role" {
--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
 }
 
