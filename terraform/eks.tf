5ce53f494c24cafbb784b4dfccec1ddda3006882
Steven Irvin <stevei101@users.noreply.github.com>
2025-10-21 13:33:57 -0500

    fix(terraform): Use kubernetes_manifest for aws-auth

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -116,30 +116,35 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
-resource "kubernetes_config_map" "aws_auth" {
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
+resource "kubernetes_manifest" "aws_auth" {
+  provider = kubernetes
 
-  data = {
-    mapRoles = yamlencode([
-      {
-        rolearn  = aws_iam_role.eks_nodegroup_role.arn
-        username = "system:node:{{EC2PrivateDNSName}}"
-        groups = [
-          "system:bootstrappers",
-          "system:nodes",
-        ]
-      },
-      {
-        rolearn  = aws_iam_role.github_actions_role.arn
-        username = "github-actions"
-        groups = [
-          "system:masters",
-        ]
-      }
-    ])
+  manifest = {
+    "apiVersion" = "v1"
+    "kind"       = "ConfigMap"
+    "metadata" = {
+      "name"      = "aws-auth"
+      "namespace" = "kube-system"
+    }
+    "data" = {
+      "mapRoles" = yamlencode([
+        {
+          "rolearn"  = aws_iam_role.eks_nodegroup_role.arn
+          "username" = "system:node:{{EC2PrivateDNSName}}"
+          "groups" = [
+            "system:bootstrappers",
+            "system:nodes",
+          ]
+        },
+        {
+          "rolearn"  = aws_iam_role.github_actions_role.arn
+          "username" = "github-actions"
+          "groups" = [
+            "system:masters",
+          ]
+        }
+      ])
+    }
   }
 
   depends_on = [
