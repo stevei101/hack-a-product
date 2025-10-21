a01f5c4c87b1e708eddaa6a4c76c87bb59075753
Steven Irvin <stevei101@users.noreply.github.com>
2025-10-21 13:00:45 -0500

    fix(terraform): Add networking for EKS node group

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -13,9 +13,10 @@ resource "aws_vpc" "eks_vpc" {
 }
 
 resource "aws_subnet" "eks_subnet_a" {
-  vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "${var.aws_region}a"
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
 
   tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
@@ -24,14 +25,46 @@ resource "aws_subnet" "eks_subnet_a" {
 }
 
 resource "aws_subnet" "eks_subnet_b" {
-  vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "${var.aws_region}b"
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "${var.aws_region}b"
+  map_public_ip_on_launch = true
 
   tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/elb"                    = "1"
   }
+}
+
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "eks-igw"
+  }
+}
+
+resource "aws_route_table" "eks_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+
+  tags = {
+    Name = "eks-route-table"
+  }
+}
+
+resource "aws_route_table_association" "eks_subnet_a_association" {
+  subnet_id      = aws_subnet.eks_subnet_a.id
+  route_table_id = aws_route_table.eks_route_table.id
+}
+
+resource "aws_route_table_association" "eks_subnet_b_association" {
+  subnet_id      = aws_subnet.eks_subnet_b.id
+  route_table_id = aws_route_table.eks_route_table.id
 }
 
 # --- IAM for EKS Cluster ---
