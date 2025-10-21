c1b69176a3a7667740d920b51df4f463e76cf27f
Steven Irvin <stevei101@users.noreply.github.com>
2025-10-20 22:07:35 -0500

    fix(terraform): add missing cloudfront distribution resource
    This resolves a 'Reference to undeclared resource' error in the IAM policy by defining the 'aws_cloudfront_distribution' 's3_distribution' resource and its dependencies.

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,3 +59,76 @@ resource "aws_ecr_repository" "backend" {
     Environment = var.environment
   }
 }
+
+# CloudFront for S3
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for static site"
+}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.site.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.oai.iam_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.site.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.site.bucket_regional_domain_name
+    origin_id   = "S3-${var.bucket_name}"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "S3 bucket distribution for ${var.bucket_name}"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${var.bucket_name}"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Name        = "Static Site CloudFront"
+    Environment = var.environment
+  }
+}
