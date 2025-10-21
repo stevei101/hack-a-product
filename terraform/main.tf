de34c2e9637b61a32fd873727c182498e4d6332e
Steven Irvin <stevei101@users.noreply.github.com>
2025-10-20 19:14:39 -0500

    security: Harden S3 bucket with public access block

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,25 @@ terraform {
 }
 
 # Configure the AWS Provider
-provider "aws" {
-  region = var.aws_region
+# S3 Bucket for Static Website Hosting
+resource "aws_s3_bucket" "site" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = "Static Site Bucket"
+    Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "site_public_access_block" {
+  bucket = aws_s3_bucket.site.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_website_configuration" "site_config" {
+  bucket = aws_s3_bucket.site.id
 }
